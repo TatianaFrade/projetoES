5721ac680e2d9bc79ba2085c24c23d09268a26f3
Tatiana Frade <2230813@my.ipleiria.pt>
2025-06-18 16:45:06 +0100

    paginaInicial

---
+++ src/main/java/Cinema.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class paginaInicial  extends JFrame {
+
+    private JPanel paginaInicial;
+
+    private JButton comprarBilheteButton;
+    private JButton comprarProdutosButton;
+    private JButton verMenusButton;
+    private JButton consultarSessõesPorDiaButton;
+    private JButton loginButton;
+
+
+
+
+}
